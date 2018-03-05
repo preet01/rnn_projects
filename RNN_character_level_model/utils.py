0a694137cf68d17e555426c3ac1db9c072265038
Harpreet <harpreet.vishnoi@gmail.com>
2018-03-05 22:42:42 +0530

    rnn

--- RNN_character_level_model/utils.py
+++ RNN_character_level_model/utils.py
@@ -9,7 +9,7 @@ def smooth(loss, cur_loss):
 
 def print_sample(sample_ix, ix_to_char):
     txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    txt = txt[0].upper() + txt[1:]  
     print ('%s' % (txt, ), end='')
 
 def get_initial_loss(vocab_size, seq_length):
@@ -22,11 +22,11 @@ def softmax(x):
 def initialize_parameters(n_a, n_x, n_y):
 
     np.random.seed(1)
-    Wax = np.random.randn(n_a, n_x)*0.01 # input to hidden
-    Waa = np.random.randn(n_a, n_a)*0.01 # hidden to hidden
-    Wya = np.random.randn(n_y, n_a)*0.01 # hidden to output
-    b = np.zeros((n_a, 1)) # hidden bias
-    by = np.zeros((n_y, 1)) # output bias
+    Wax = np.random.randn(n_a, n_x)*0.01 
+    Waa = np.random.randn(n_a, n_a)*0.01 
+    Wya = np.random.randn(n_y, n_a)*0.01 
+    b = np.zeros((n_a, 1)) 
+    by = np.zeros((n_y, 1)) 
     
     parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
     
@@ -35,17 +35,16 @@ def initialize_parameters(n_a, n_x, n_y):
 def rnn_step_forward(parameters, a_prev, x):
     
     Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
-    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
-    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars 
-    
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)
+    p_t = softmax(np.dot(Wya, a_next) + by) 
     return a_next, p_t
 
 def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
     
     gradients['dWya'] += np.dot(dy, a.T)
     gradients['dby'] += dy
-    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
-    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next']
+    daraw = (1 - a * a) * da 
     gradients['db'] += daraw
     gradients['dWax'] += np.dot(daraw, x.T)
     gradients['dWaa'] += np.dot(daraw, a_prev.T)
@@ -63,26 +62,20 @@ def update_parameters(parameters, gradients, lr):
 
 def rnn_forward(X, Y, a0, parameters, vocab_size = 27):
     
-    # Initialize x, a and y_hat as empty dictionaries
     x, a, y_hat = {}, {}, {}
     
     a[-1] = np.copy(a0)
     
-    # initialize your loss to 0
     loss = 0
     
     for t in range(len(X)):
         
-        # Set x[t] to be the one-hot vector representation of the t'th character in X.
-        # if X[t] == None, we just have x[t]=0. This is used to set the input for the first timestep to the zero vector. 
         x[t] = np.zeros((vocab_size,1)) 
         if (X[t] != None):
             x[t][X[t]] = 1
         
-        # Run one step forward of the RNN
         a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
         
-        # Update the loss by substracting the cross-entropy term of this time-step from it.
         loss -= np.log(y_hat[t][Y[t],0])
         
     cache = (y_hat, a, x)
@@ -90,19 +83,16 @@ def rnn_forward(X, Y, a0, parameters, vocab_size = 27):
     return loss, cache
 
 def rnn_backward(X, Y, parameters, cache):
-    # Initialize gradients as an empty dictionary
     gradients = {}
     
-    # Retrieve from cache and parameters
     (y_hat, a, x) = cache
     Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
     
-    # each one should be initialized to zeros of the same dimension as its corresponding parameter
     gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
     gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
     gradients['da_next'] = np.zeros_like(a[0])
     
-    # Backpropagate through time
+    
     for t in reversed(range(len(X))):
         dy = np.copy(y_hat[t])
         dy[Y[t]] -= 1
